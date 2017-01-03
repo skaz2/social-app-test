43c681958e6a7c7b95cdee44793c30df9ddf1ed2
skaz <admin@Mac-Admin.local>
2017-01-03 12:39:29 +0300

    Add profile image

--- social-app-test/FeedVC.swift
+++ social-app-test/FeedVC.swift
@@ -10,17 +10,25 @@ import UIKit
 import Firebase
 import SwiftKeychainWrapper
 
-class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var imageAdd: CircleView!
     
     var posts = [Post]()
+    var imagePicker: UIImagePickerController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         tableView.delegate = self
         tableView.dataSource = self
+        
+        imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+        
+        
         
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
             if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
@@ -59,6 +67,15 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         }
 
     }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+           imageAdd.image = image
+        } else {
+            print("INFO: A valid image wasn't selected")
+        }
+        imagePicker.dismiss(animated: true, completion: nil)    }
 
     
     @IBAction func signOutTapped(_ sender: Any) {
@@ -67,6 +84,13 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         print("INFO: ID removed from keychain \(keychainResult)")
         try! FIRAuth.auth()?.signOut()
         performSegue(withIdentifier: "goToSignIn", sender: nil)
+    }
+    
+    
+    @IBAction func addImageTapped(_ sender: Any) {
+        present(imagePicker, animated: true, completion: nil)
+        
+        
     }
 
 
